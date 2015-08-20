964376f251c21499b945ab30e6cbc1bc458926c7
inDuberitably <scottjduberstein@gmail.com>
2015-08-20 13:16:15 -0400

    Delete EmailRequestForm.rb

--- bots/EmailRequestForm.rb
+++
@@ -1,8 +0,0 @@
-class EmailRequestForm
-  class << self
-    attr_accessor :sender, :email_id, :original_msg, :hours, :completed, :total_hours
-    def to_s
-      return "Sender:#{self.sender}\nEmail_id:#{self.email_id}\nOriginal_msg:#{self.original_msg}\nHours:#{self.hours}\nCompleted?:#{self.completed}\n"
-    end
-  end
-end
