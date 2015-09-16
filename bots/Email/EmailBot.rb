1a6d9e89cab6a0727943309b61afd025a33911be
inDuberitably <scottjduberstein@gmail.com>
2015-09-15 20:07:53 -0400

    Update EmailBot.rb

--- bots/Email/EmailBot.rb
+++ bots/Email/EmailBot.rb
@@ -5,11 +5,12 @@ class EmailBot
     @@cases= File.open('subjects.txt').read.split("\n")
     @@entire_phrases= File.open('phrases.txt').read.split("\n").each{|y| y.gsub!(/\s+/, '_')}
     @@display_phrases = ""
+    @@commands = File.open('commands.txt').read.split("\n")
     a = ""
     list = File.read('email_list.txt').scan(/[^\s+]+/).each {|y| a<<y}
+    @@address_book = Mail::AddressList.new(a)
     @@config_pop3 = File.read('pop3.txt').split("\n")
     config_smtp = File.read('smtp.txt').split("\n")
-    @@address_book = Mail::AddressList.new(a)
 
     @@entire_phrases.each do |w|
       @@display_phrases+= w.gsub(/_/, " ")
@@ -18,7 +19,6 @@ class EmailBot
       end
     end
     @@time_slot_hsh = {}
-    @@i = 0
     @@pop=Mail.defaults do
       retriever_method :pop3, :address    => @@config_pop3[0],
         :port       => @@config_pop3[1],
@@ -31,31 +31,62 @@ class EmailBot
                 :port                 => config_smtp[1],
                 :user_name            => config_smtp[2],
                 :password             => config_smtp[3],
-                :authentication       => 'plain', #'login' for Office365
+                :authentication       => 'login', #'login' for Office365
                 :encryption => 'tls',
                 :enable_starttls_auto => true  }
     Mail.defaults do
       delivery_method :smtp, options
     end
   end
-  def check_inbox()
-    puts @@i
-    @@i += 1
 
-    @@time_slot_hsh.values.select{|request| request.completed == false }.each do |requests|
-      puts requests
+  def purge_completed(email)
+    @@time_slot_hsh.delete_if{|key,request| request.completed == true}
+    send_email(email,"Purged completed requests.")
+  end
+
+  def show_requests()
+    if !@@time_slot_hsh.values.empty?
+      @@time_slot_hsh.values.select{|request| request.completed == false }.each do |requests|
+        puts requests
+      end
+    else
+      puts "{}"
     end
+  end
 
+  def status(email)
+    status = []
+    @@time_slot_hsh.values.each do |requests|
+      status << requests
+    end
+    send_email(email,status)
+  end
+
+  def check_inbox()
+    show_requests()
     hours =  inbox()
     if hours.empty?
-      puts "Process is asleep"
-      sleep(30)
+      puts "No requests as of #{Time.now}."
     else
       hours.each do |requested_time|
-        send_mail(requested_time,false)
+        send_requests(requested_time,false)
       end
     end
-    return check_inbox()
+  end
+=begin
+meant for one user to manage internals
+=end
+  def send_email(email,command)
+    send_message = command
+    mail = Mail.deliver do
+      from    @@config_pop3[4]
+      to       email.from
+      subject  email.subject
+      text_part do
+        body send_message
+      end
+    end
+
   end
 
   def available_hours(msg)
@@ -69,7 +100,7 @@ Grabs time slots
 =begin
 Writes response to a given request
 =end
-  def send_mail(requested_time,flag)
+  def send_requests(requested_time,flag)
     msg = requested_time.parts[0].body.decoded
     hours = available_hours(msg)
     send_message = ""
@@ -85,7 +116,8 @@ Is this a response or new request?
 
         If you can cover the ENTIRE shift respond with one of the following phrases: #{@@display_phrases}
 
-        If you can cover a portion of the shift respond using: [ ] and enclose the times are able to cover this shift)
+        If you can cover a portion of the shift respond using: [ ] and enclose the times are able to cover this shift
+      )
 
       store_msg(requested_time.from,subj,msg,hours,false)
     elsif @@time_slot_hsh.has_key? requested_time.subject.gsub(/[^(\d+(...)+)]/,"") and flag == false
@@ -94,6 +126,8 @@ Is this a response or new request?
       #Grabs everything between brackets
       msg.scan(/\[(.*?)\]/).each{|c| c.each {|d| send_message<< d.strip}}
       send_message += "\n Partial time awarded to #{requested_time.from[0]}"
+
+
     else
       subj = requested_time.subject
       send_message = "Hours are awarded to #{requested_time.from}\n#{msg}"
@@ -131,15 +165,16 @@ Reads email responses
 key phrases to select mail with as to prevent spam
 =end
     hours = []
-    @@pop.all.each do |email|
+    mail_box = @@pop.all.select{|x| !x.nil?}
+    mail_box.each do |email|
 =begin
 Checks for NEW time off requests
 examples: "I need time off for today", "Hours for next week", "I need someone to cover my shift"
 =end
-      header = @@cases & email.subject.downcase.scan(/\w+/).each {|word| word.strip!}
+      header = @@cases + @@commands & email.subject.downcase.scan(/\w+/).each {|word| word.strip!}
       date_key =  email.subject.gsub(/[^(\d+(...)+)]/,"")
       content = email.body.parts[0].body.decoded
-      if header.length >= 1 and !@@time_slot_hsh.has_key? date_key
+      if @@cases.any?{|sub| sub.include? header[0]} and !@@time_slot_hsh.has_key? date_key
         hours << email
         puts "You've got mail with subject headers"
 =begin
@@ -159,26 +194,31 @@ Responder can cover all hours
         if !content.empty? and @@entire_phrases.any?{|phrase| phrase.include? content[0]}
           puts "Awarding hours..."
           @@time_slot_hsh.fetch(date_key).completed = true
-          self.send_mail(email,true)
+          send_requests(email,true)
 =begin
 Responder can cover a portion of the shift.
 =end
         elsif partial_content.length >= 2
-          send_mail(email,false)
+          send_requests(email,false)
         else
-          puts "What the family dude he funked up big time."
+          puts "Invalid format."
         end
-
+=begin
+An email was sent as a command
+=end
+      elsif  @@commands.any?{|phrase| phrase.include? header[0]}
+        if header.include? "status"
+          status(email)
+        elsif header.include? "purge"
+          purge_completed(email)
+        end
 =begin
 An email was sent, but didn't meet the requirements
 =end        
       else
-        puts "I'm in the kitchen cooking pies with my baby."
+        puts "Email without valid subject headers"
       end
     end
     return hours
   end
 end
-em = EmailBot.new()
-
-puts em.check_inbox
